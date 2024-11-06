2def2da2ac1f0d45d1bfd6bee23da6c20b7a2810
Yurii921 <89513539+Yurii921@users.noreply.github.com>
2024-11-06 20:49:59 +0000

    test: add the test for estimating deployment gas cost

---
+++ test/estimate-deployment.ts
@@ -0,0 +1,12 @@
+import hre, { ignition } from "hardhat";
+
+import Module from "../ignition/modules/MagnifyCashCollateralNFT";
+
+describe("Deployment", () => {
+    if (hre.network.name !== "hardhat" && hre.network.name !== "base-local" && hre.network.name !== "localhost")
+        throw new Error("Error. This script is assumed to run on a forked network or locally");
+
+    it("Deploys all the contracts", async () => {
+        await ignition.deploy(Module);
+    });
+});
