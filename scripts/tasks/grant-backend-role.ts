0b2601c92eeb29abe8ada7933d3f60d799ff4611
Yurii921 <89513539+Yurii921@users.noreply.github.com>
2024-11-07 02:23:36 +0000

    chore(grant-backend-role-task): use the Ignition modules

--- scripts/tasks/grant-backend-role.ts
+++ scripts/tasks/grant-backend-role.ts
@@ -2,7 +2,11 @@
 
 import { scope } from "hardhat/config";
 
-import { getParamByKey, readDeployedAddress, requireBaseChain, requireChainID } from "../helpers";
+import SBTModule from "../../ignition/modules/MagnifyCashSBT";
+import CollateralNFTModule from "../../ignition/modules/MagnifyCashCollateralNFT";
+import { getParamByKey } from "../helpers";
+
+import { MagnifyCashCollateralNFT, MagnifyCashSBT } from "../../typechain-types";
 
 const grantingScope = scope("grant-backend", "Grant the backend role to a specified account");
 
@@ -10,36 +14,34 @@ grantingScope
     .task("MagnifyCashSBT", "Grant the backend role to a specified account")
     .addParam("account", "An account which is granted the backend role")
     .setAction(async (taskArgs, hre) => {
-        const { ethers, network, run } = hre;
+        const { run, ethers, ignition } = hre;
         await run("compile");
 
         const [defaultAdmin] = await ethers.getSigners();
 
-        requireBaseChain(network);
-
-        const chainID = requireChainID(network);
-        const contractAddress = await readDeployedAddress(chainID, "MagnifyCashSBT#MagnifyCashSBT");
-        const contract = await ethers.getContractAt("MagnifyCashSBT", contractAddress);
+        const { sbt: sbtContract } = await ignition.deploy(SBTModule);
+        const sbt = sbtContract as unknown as MagnifyCashSBT;
 
         const backendAddress = getParamByKey(taskArgs as object, "account");
-        await contract.connect(defaultAdmin).grantRole(await contract.BACKEND_ROLE(), backendAddress);
+        await sbt.connect(defaultAdmin).grantRole(await sbt.BACKEND_ROLE(), backendAddress);
+
+        console.log("\nGranted.\n");
     });
 
 grantingScope
     .task("MagnifyCashCollateralNFT", "Grant the backend role to a specified account")
     .addParam("account", "An account which is granted the backend role")
     .setAction(async (taskArgs, hre) => {
-        const { ethers, network, run } = hre;
+        const { run, ethers, ignition } = hre;
         await run("compile");
 
         const [defaultAdmin] = await ethers.getSigners();
 
-        requireBaseChain(network);
-
-        const chainID = requireChainID(network);
-        const contractAddress = await readDeployedAddress(chainID, "MagnifyCashCollateralNFT#MagnifyCashCollateralNFT");
-        const contract = await ethers.getContractAt("MagnifyCashCollateralNFT", contractAddress);
+        const { collateralNFT: collateralNFTContract } = await ignition.deploy(CollateralNFTModule);
+        const collateralNFT = collateralNFTContract as unknown as MagnifyCashCollateralNFT;
 
         const backendAddress = getParamByKey(taskArgs as object, "account");
-        await contract.connect(defaultAdmin).grantRole(await contract.BACKEND_ROLE(), backendAddress);
+        await collateralNFT.connect(defaultAdmin).grantRole(await collateralNFT.BACKEND_ROLE(), backendAddress);
+
+        console.log("\nGranted.\n");
     });
